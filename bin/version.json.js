341dae8ab29225ad881871598ab01aea052c16dc
zusterben <zusterben1s@hotmail.com>
2024-08-19 11:51:47 +0800

    修复bcm470x支持

--- bin/version.json.js
+++ bin/version.json.js
@@ -1,12 +1,12 @@
 {
   "name": "helloworld",
-  "version": "0.9.8",
-  "md5_arm_full": "543074f9890b02328a337d139cf49cec",
-  "md5_arm_lite": "5fed85bed5181a6f8e92ffd3a73f4b7a",
-  "md5_armng_full": "0f2a5f1dd8fcff5c4c1aa7bd7a530d32",
-  "md5_armng_lite": "48ada92d4ec57c5190a6b0d9e8d3fa95",
-  "md5_arm64_full": "8dae8d6a2b28ab7075738967bca0ae79",
-  "md5_arm64_lite": "d5f8cce55f7a8459f81edfa94c4ca1fa",
-  "md5_mips_lite": "d4dca8e8f08c2b3e3a94a8480e7568e5",
-  "md5_mipsle_lite": "fdb1b6edfac262a3e24e754a781b7316"
+  "version": "0.9.9",
+  "md5_arm_full": "fcafe1991373a527bfaa2a44f818fd90",
+  "md5_arm_lite": "ee0c002597d72cca5956765e2809d9ff",
+  "md5_armng_full": "9d0c3d895e31c0c1312fe937c2240258",
+  "md5_armng_lite": "554ccfd05770056a0872041066f8b44f",
+  "md5_arm64_full": "6c92e27642669febae2bcbca9edf178a",
+  "md5_arm64_lite": "03f1a77cecac6ea77bba85f868404802",
+  "md5_mips_lite": "b265bd2be1dbbe87a642bc3ac25d70b5",
+  "md5_mipsle_lite": "5af08ad8ca3b9c217d40b3d9f878a277"
 }
