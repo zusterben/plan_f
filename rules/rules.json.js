6a22e7a5312d5ac63603df9d6fc8867d3143920a
zusterben <zusterben1s@hotmail.com>
2025-11-26 21:20:51 +0800

    update rules

--- rules/rules.json.js
+++ rules/rules.json.js
@@ -1,23 +1,23 @@
 {
   "gfwlist": {
     "name": "gfwlist.conf",
-    "date": "2025-07-07 15:22",
-    "md5": "aa798d6d2fd46027c6bf282910b187ac",
-    "count": "5473"
+    "date": "2025-11-26 21:20",
+    "md5": "b15bfe43cfded7cb295348eeb258a33a",
+    "count": "5567"
   },
   "chnroute": {
     "name": "chnroute.txt",
-    "date": "2025-07-07 15:22",
-    "md5": "eb10b35aefa24dd4fdca6a8e3ef025c7",
-    "count": "7449",
-    "count_ip": "498570346492",
+    "date": "2025-11-26 21:20",
+    "md5": "bde108266596b70a2bedb26e61721daf",
+    "count": "7135",
+    "count_ip": "764857004802",
     "source": "ipip",
     "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
   },
   "cdn_china": {
     "name": "cdn.txt",
-    "date": "2025-07-07 15:22",
-    "md5": "5491467de7f22c9fc8d4a9a180e4b327",
-    "count": "120229"
+    "date": "2025-11-26 21:20",
+    "md5": "26821f559e23f07c4974976298045ca5",
+    "count": "116470"
   }
 }
