a373abcf90154153e03eac6562fb2f79affb36f6
zusterben <zusterben1s@hotmail.com>
2026-03-07 22:47:52 +0800

    xray二进制回滚1.8.9版本

--- bin/version.json.js
+++ bin/version.json.js
@@ -1,12 +1,12 @@
 {
   "name": "helloworld",
-  "version": "1.0.6",
-  "md5_arm_full": "be357ca9ad2a4c1ef5ebaac7654b9fd6",
-  "md5_arm_lite": "5e400c7fce444ef80e34d1ca6c4eeff5",
-  "md5_armng_full": "5413467514aa74feee8003c3f69c3e35",
-  "md5_armng_lite": "e344eeeffbb253124108727ae699ad6f",
-  "md5_arm64_full": "1efaf710eb7dda055c35bb35dab09e78",
-  "md5_arm64_lite": "cba75e8549acb6519742897bf31b4f48",
-  "md5_mips_lite": "2f3e30eb993adbada148c429264ec419",
-  "md5_mipsle_lite": "1887ab64f784e5f11401ada044502cbc"
+  "version": "1.0.7",
+  "md5_arm_full": "fe03e8548a6954c7302712cff7e1ba06",
+  "md5_arm_lite": "588ae135c0ac9f16241a0610c87e1286",
+  "md5_armng_full": "c60ab8986cb7d7df26b33fdb224df3dc",
+  "md5_armng_lite": "8e7ed5942adf3b30ba3684c738869f58",
+  "md5_arm64_full": "63ef7221f6afef6136021b2761bcb5f9",
+  "md5_arm64_lite": "1310f12af8613d495dab3fc635bebd54",
+  "md5_mips_lite": "98aa97d0944c38124c29b9b871e752ef",
+  "md5_mipsle_lite": "3822ea58e9fc6c1d79fb82ef55ecca5d"
 }
